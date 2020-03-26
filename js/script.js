28689aba92e225ae6f08160a0927776322ca778d
farrux3184 <59306883+farrux3184@users.noreply.github.com>
2020-03-26 14:19:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+alert('lessonJS_001');
+
+
+const money = 'lessonJS-001';
+let income = 20;
+const addExpenses = 30;
+let deposit = 40;
+const monmission = 50;
+let period = 60;
+console.log (money);
+console.log (income);
+console.log (addExpenses);
+console.log (deposit);
+console.log (monmission);
+console.log (period);
