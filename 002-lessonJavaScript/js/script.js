6fae231d4e3e75fbe8c7077fcc3205a2d5c2f380
farrux3184 <59306883+farrux3184@users.noreply.github.com>
2020-03-27 22:29:35 +0300

    lessonJS-002

---
+++ 002-lessonJavaScript/js/script.js
@@ -0,0 +1,33 @@
+const money = 300000;
+let income = 'фриланс';
+let addExpenses = 'интернет,продукты,лекарства,проезд,мобильная связь';
+let deposit = 10000
+const mission = 1800000;
+let period = 6;
+const budgetDay = 10000
+console.log (money);
+console.log (income);
+console.log(deposit === 20000);
+// вывод числа символов в переменной addExpenses
+console.log(addExpenses.length);
+// умножение двух переменных и вывод в консоль
+Mission = period * money;
+console.log(Mission, 'рублей');
+console.log(addExpenses.split(','));
+MissionMonth = 30 * budgetDay;
+console.log(MissionMonth, 'рублей');
+
+// выводит переменную add в нижнем регисторе
+// console.log(add.toLowerCase(''));
+// делит слова на массив
+// console.log(add.split(' '));
+// console.log(budgetDay);
+
+// разбивает предложение на массивы
+// console.log(???.split(''));
+// свойство для вывода слов в верхнем регисторе
+// внутренние ковычки обязательны
+// console.log(???.toUpperCase());
+// свойство для вывода слов в нижнем регисторе
+// console.log(???.toLowerCase()); 
+// внутренние ковычки обязательны
