d4c7dba741d692dae55e943467f72b8afc1115b5
farrux djanbaev <farrux3184@mail.ru>
2020-04-24 12:41:56 +0300

    2222

--- js/script.js
+++ js/script.js
@@ -84,7 +84,6 @@ let appData = {
         allInput[5].value = '';
         allInput[7].value = '';
         allInput[9].value = '';
-        allInput[15].value = '';
     },
     // вывод результата
     showResult: function () {
