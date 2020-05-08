17b1f817a8f298148e1355f914d719c682ff1239
farrux djanbaev <farrux3184@mail.ru>
2020-05-08 11:59:25 +0300

    v2

--- 3D module/scripts/script.js
+++ 3D module/scripts/script.js
@@ -253,15 +253,59 @@ const slider = () => {
 };
 slider();
 // CALCULATED
-const calculated = () => {
-  const inputNumber = document.querySelectorAll('.calc-item');
+const calc = (price = 100) => {
+  const inputNumber = document.querySelectorAll('.calc-item'),
+      calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcDay = document.querySelector('.calc-day'),
+      calcCount = document.querySelector('.calc-count'),
+      totalValue = document.getElementById('total');
+
   inputNumber.forEach(el => {
     el.addEventListener('input', () => {
       el.value = el.value.replace(/[^0-9]/, '');
     });
   });
+  const countSum = () => {
+    let total = 0,
+    countValue = 1,
+    dayValue = 1;
+    const tapeValue = calcType.options[calcType.selectedIndex],
+     squareValue = +calcSquare.value;
+console.log(tapeValue);
+     if(calcCount.value > 1){
+       countValue += (calcCount.value -1) / 10;
+      }
+
+      if(calcDay.value && calcDay.value < 5){
+        dayValue  *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+     if (tapeValue && squareValue){
+       total = price * tapeValue * squareValue * countValue * dayValue;
+     } 
+    totalValue.textContent = total;
+  };
+
+    calcBlock.addEventListener('change', (event) => {
+      const target = event.target;
+      // if (target.matches('.calc-type') || target.matches('.calc-square') ||
+      // target.matches('.calc-day') || target.matches('.calc-count')){
+      //   console.log(123);
+      // }
+      // if (target === calcType || target === calcSquare ||
+      //    target ===  calcDay || target === calcCount){
+      //      console.log(123);
+      //    }
+      if(target.matches('select') || target.matches('input')){
+        countSum();
+      }
+    });
 };
-calculated();
+calc(100);
 // COMMANDS
 const command = () => {
   const command = document.querySelector('.command'),
