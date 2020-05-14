4e3b8e3445d8b05344f33339e3320446394678ab
farrux3184 <farrux3184@mail.ru>
2020-05-14 18:10:14 +0300

    new start

--- scripts/script.js
+++ scripts/script.js
@@ -330,13 +330,13 @@ let att;
     })
   });
 };
-  command();
+command();
 
   // VALIDATED
   const valid = new Validator({
     selector: '#form1',
     pattern: {
-      cirilic: /^[а-яА-ЯёЁa]+$/,
+      cirilic: /^[а-яА-ЯёЁa '']+$/,
       phone: /^\+?[78]([-()]*\d){10}$/,
       email: /^\w+@\w+\.\w{2,}$/
     },
@@ -359,7 +359,7 @@ let att;
   const valid2 = new Validator({
     selector: '#form2',
     pattern: {
-      cirilic: /^[а-яА-ЯёЁa]+$/,
+      cirilic: /^[а-яА-ЯёЁa '']+$/,
       phone: /^\+?[78]([-()]*\d){10}$/,
       email: /^\w+@\w+\.\w{2,}$/
     },
@@ -386,7 +386,7 @@ let att;
 const valid3 = new Validator({
   selector: '#form3',
   pattern: {
-    cirilic: /^[а-яА-ЯёЁa]+$/,
+    cirilic: /^[а-яА-ЯёЁa '']+$/,
     phone: /^\+?[78]([-()]*\d){10}$/,
     email: /^\w+@\w+\.\w{2,}$/
   },
@@ -407,18 +407,57 @@ const valid3 = new Validator({
 });
 valid3.init();
 // SEND-ajax-form
-const sendForm = () => {
+  const form1 = document.getElementById('form1'),
+    form2 = document.getElementById('form2'),
+    form3 = document.getElementById('form3');
+  const sendForm = (form) => {
   const errorMessag = 'Что то пошло не так...',
     loadMessage = 'Загрузка...',
     successMessage = 'Спасибо! Мы с вами свяжемся!';
-  const form = document.getElementById('form1');
-
   const statusMessage = document.createElement('div');
-  statusMessage.textContent = 'Тут будет сообщение!';
   statusMessage.style.cssText = 'font-size: 2rem';
-  form.appendChild(statusMessage);
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    form.appendChild(statusMessage);
+    statusMessage.textContent = loadMessage;
+    const formData = new FormData(form);
+    
+//     let body = {};
+//   // for(let val of formData.entries()){
+//   //   body[val[0]] = val[1]
+//   // }
+// formData.forEach((val, key) => {
+//       body[key] = val;
+//   });
+  postData(formData).then((response) => {
+    if(response.status !== 200){
+      throw new Error('status network not 200')
+    }
+    statusMessage.textContent = successMessage;
+  }).catch(error => {
+    statusMessage.textContent = errorMessag;
+    console.error(error);
+  });
+});
+const postData = (formData) => {
+  return fetch('./server.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    // body: JSON.stringify(body)
+    body: formData
+
+  });
 };
-sendForm();
+};
+sendForm(form1);
+sendForm(form2);
+sendForm(form3);
+
+
+
 
 });
 
