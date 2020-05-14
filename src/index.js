8b56a3e867462079a88ab2cd894b694bb390de2b
farrux3184 <farrux3184@mail.ru>
2020-05-14 19:30:27 +0300

    new babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// import 'nodelist-foreach-polyfill';
+
 import countTimer from './modules/countTimer';
 import toogleMenu from './modules/toogleMenu';
 import togglePopUp from './modules/togglePopUp';
